bf8a8735dd8efd8a3c358c3d5724f6bbc6d9a709
Mohammadreza <ma3968312@gmail.com>
2025-02-21 22:01:46 +0330

    set query in category page

--- components/templates/CategoriesPage.js
+++ components/templates/CategoriesPage.js
@@ -1,23 +1,43 @@
-import styles from "./Categories.module.css";
+import { useState } from "react";
+import styles from "./CategoriesPage.module.css";
+import { useRouter } from "next/router";
 
 function CategoriesPage() {
+  const router = useRouter();
+  const [query, setQuery] = useState({ difficulty: "", time: "" });
+
+  const changeHandler = (event) => {
+    setQuery({ ...query, [event.target.name]: event.target.value });
+  };
+
+  const searchHandler = () => {
+    router.push({
+      pathname: "/categories",
+      query,
+    });
+  };
+
   return (
     <div className={styles.container}>
       <h2>Categories</h2>
       <div className={styles.subContainer}>
         <div className={styles.select}>
-          <select>
-            <option>Difficulty</option>
-            <option>Easy</option>
-            <option>Medium</option>
-            <option>Hard</option>
+          <select
+            value={query.difficulty}
+            name="difficulty"
+            onChange={changeHandler}
+          >
+            <option value="">Difficulty</option>
+            <option value="Easy">Easy</option>
+            <option value="Medium">Medium</option>
+            <option value="Hard">Hard</option>
           </select>
-          <select>
-            <option>Cooking Time</option>
-            <option>More than 30 min</option>
-            <option>Less than 30 min </option>
+          <select value={query.time} name="time" onChange={changeHandler}>
+            <option value="">Cooking Time</option>
+            <option value="more">More than 30 min</option>
+            <option value="less">Less than 30 min </option>
           </select>
-          <button>Search</button>
+          <button onClick={searchHandler}>Search</button>
         </div>
       </div>
     </div>
