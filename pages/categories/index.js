a44a32942089bfc864dad541a12fd6d41a5fdaf2
Mohammadreza <ma3968312@gmail.com>
2025-02-21 20:10:59 +0330

    create categories page and style

---
+++ pages/categories/index.js
@@ -0,0 +1,8 @@
+import CategoriesPage from "@/components/templates/CategoriesPage";
+import React from "react";
+
+function Categories() {
+  return <CategoriesPage />;
+}
+
+export default Categories;
